5a56e07a3d92f17987ee3d1f7790c9e37a60a61b
dhamie <dhamie.soyemi@gmail.com>
2018-04-10 02:42:09 +0100

    cool

---
+++ code.php
@@ -0,0 +1,85 @@
+
+<?php
+require 'link.php';
+//require 'api.php';
+
+
+
+  if (isset($_POST['submit'])){
+  $fname=addslashes($_POST['firstname']);
+  $lname=addslashes($_POST['lastname']);
+  $country=($_POST['country']);
+  $phn=($_POST['phone']);
+  $email=($_POST['email']);
+  $uname=($_POST['user']);
+  $pword=md5($_POST['password1']);
+  $repword=md5($_POST['password2']);
+  $currency=($_POST['currency']);
+  
+
+
+  $date = date('Y-m-d');
+
+  $sql1="SELECT * FROM register WHERE username = '$uname'";
+    $query=mysqli_query($connection, $sql1);
+
+
+    if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
+
+      echo "invalid email";
+
+    }else if(mysqli_num_rows($query) > 0){
+
+       echo "username taken"; 
+      
+    }else if($pword != $repword){
+
+       echo "passwords dont match"; 
+      
+    }else{
+
+  
+  $sql="INSERT INTO `register`(`dateor`, `fname`, `lname`, `country`, `phn`, `email`, `username`, `password`, `currency`) VALUES ('$date','$fname','$lname','$country','$phn','$email','$uname','$pword','$currency');"; 
+
+
+  $res = mysqli_multi_query($connection, $sql);
+  if($res == 1){
+
+        header('location:login.htm');   
+      }else{
+        echo mysqli_error($connection);
+
+     }
+}
+}
+
+
+if (isset($_POST['submit1'])){
+  $user=$_POST['user'];
+  $pword=md5($_POST['password']);
+
+
+
+  $sql="SELECT * FROM `register` WHERE `username`='".$user."' AND `password`='".$pword."'";
+  $res=mysqli_query($connection, $sql);
+  echo mysqli_error($connection);
+  if(mysqli_num_rows($res) == 1){
+    if(isset($_POST['remember'])){
+      setcookie('user', $user, time()+60*60*7);
+      setcookie('pword', $pword, time()+60*60*7);
+      }
+      session_start();
+      $_SESSION['user'] =$user;
+      //$_SESSION['time'] =time();
+      header('location:dash.php');
+    
+  }else{
+          echo "Username or Password Incorect";
+
+    
+  }
+
+}
+
+
+?>
---
+++ dash.php
@@ -0,0 +1,359 @@
+<?php
+
+require 'link.php';
+session_start();
+
+$user=$_SESSION['user'];
+//echo $user;
+
+?>
+<!DOCTYPE html>
+<!-- saved from url=(0043)https://Keystockoptions.com/user/index.php -->
+<html style="height: auto; min-height: 100%;"><style type="text/css" id="night-mode-pro-style"></style><link type="text/css" rel="stylesheet" id="night-mode-pro-link"><script async="" src="./files/default" charset="UTF-8" crossorigin="*"></script><script id="tinyhippos-injected">if (window.top.ripple) { window.top.ripple("bootstrap").inject(window, document); }</script><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Ulitimate Traders Binary Options</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="./files/bootstrap.min.css">
+  <link rel="stylesheet" href="./files/font-awesome.min.css">
+  <link rel="stylesheet" href="./files/ionicons.min.css">
+  <link rel="stylesheet" href="./files/jquery-jvectormap.css">
+  <link rel="stylesheet" href="./files/AdminLTE.min.css">
+  <link rel="stylesheet" href="./files/_all-skins.min.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="./files/css">
+<style type="text/css">.jqstooltip { position: absolute;left: 0px;top: 0px;visibility: hidden;background: rgb(0, 0, 0) transparent;background-color: rgba(0,0,0,0.6);filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);-ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)";color: white;font: 10px arial, san serif;text-align: left;white-space: nowrap;padding: 5px;border: 1px solid white;box-sizing: content-box;z-index: 10000;}.jqsfield { color: white;font: 10px arial, san serif;text-align: left;}</style></head>
+	<!-- Use this ile to set the theme of the web app -->
+<body class="skin-blue sidebar-mini" style="height: auto; min-height: 100%; zoom: 1;">	<div class="wrapper" style="height: auto; min-height: 100%;">
+		<header class="main-header">
+  <!-- Logo -->
+  <a href="https://Keystockoptions.com/user/index.php" class="logo">
+    <!-- mini logo for sidebar mini 50x50 pixels -->
+    <span class="logo-mini"><b>U</b> - T</span>
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg"><b>Ultimate</b> Traders</span>
+  </a>
+
+  <!-- Header Navbar: style can be found in header.less -->
+  <nav class="navbar navbar-static-top">
+    <!-- Sidebar toggle button-->
+    <a href="https://Keystockoptions.com/user/index.php#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+      <span class="sr-only">Toggle navigation</span>
+    </a>
+    <!-- Navbar Right Menu -->
+    <div class="navbar-custom-menu">
+      <ul class="nav navbar-nav">
+        <li>
+          <a href="logout.php"><i class="fa fa-sign-out"></i> Logout</a>
+        </li>
+      </ul>
+    </div>
+
+  </nav>
+</header><aside class="main-sidebar">
+	<!-- sidebar: style can be found in sidebar.less -->
+	<section class="sidebar" style="height: auto;">
+		<!-- Sidebar user panel -->
+		<div class="user-panel">
+			<div class="pull-left image">
+				<img src="./files/avat.png" class="img-circle" alt="User Image">
+			</div>
+			<div class="info">
+				<p>Zee Zee </p>
+				<small>Welcome Dear <?php echo $user;?></small>
+			</div>
+		</div>
+		<ul class="sidebar-menu tree" data-widget="tree">
+			<li class="active">
+				<a href="https://Keystockoptions.com/user/index.php">
+					<i class="fa fa-dashboard"></i> <span>Dashboard</span>
+				</a>
+			</li>
+			<li>
+				<a href="https://Keystockoptions.com/user/fund.php">
+					<i class="fa fa-arrow-circle-up"></i> <span>Fund Account</span>
+				</a>
+			</li>
+			<li>
+				<a href="https://Keystockoptions.com/user/withdraw.php">
+					<i class="fa fa-arrow-circle-down"></i> <span>Withdraw</span>
+				</a>
+			</li>
+			<li>
+				<a href="https://Keystockoptions.com/user/tradeHistory.php">
+					<i class="fa fa-th"></i> <span>Trade History</span>
+				</a>
+			</li>
+			<li>
+				<a href="https://Keystockoptions.com/user/accountDetails.php">
+					<i class="fa fa-user"></i> <span class="xn-text">Account Details</span>
+				</a>
+			</li>
+			<li>
+				<a href="https://Keystockoptions.com/user/verify.php"><span class="fa fa-ban"></span> <span class="xn-text">Verify Account</span></a>				<!-- <a href="verify.php">
+					<i class="fa fa-check"></i> <span class="xn-text">Account Verified</span>
+				</a> -->
+			</li>
+			<li>
+				<a href="https://Keystockoptions.com/user/feedback.php">
+					<i class="fa fa-clipboard"></i> <span class="xn-text">Send Feedback</span>
+				</a>
+			</li>
+			<li>
+				<a href="https://Keystockoptions.com/user/update_profile.php">
+					<i class="fa fa-user"></i> <span class="xn-text">Update Profile</span>
+				</a>
+			</li>
+		</ul>
+	</section>
+	<!-- /.sidebar -->
+</aside>
+
+
+		<!-- Content Wrapper. Contains page content -->
+		<div class="content-wrapper" style="min-height: 818px;">
+			<!-- Content Header (Page header) -->
+
+			<section class="content-header">
+		<div class="row">
+		<div class="col-md-12">
+			<div class="alert alert-danger alert-dismissable">
+				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+				<i class="fa fa-info"></i> Oh snap! Looks like you are yet to verify your account. Verify <a href="https://Keystockoptions.com/user/verify.php" style="font-weight:bold;">Here</a>
+			</div>		</div>
+	</div>
+		<div class="row">
+		<div class="col-md-12">
+			<ol class="breadcrumb">
+				<li><i class="fa fa-home"></i> Home</li>
+								<li class="active">Dashboard</li>
+			</ol>
+		</div>
+	</div>
+</section>
+			<!-- Main content -->
+			<section class="content">
+				<!-- Info boxes -->
+								<div class="row" style="position: relative;">
+<div style="margin:0 auto;background-color:#DFDFDF;width:100%;height:28px;line-height:26px;border:1px solid #DFDFDF;color:#000000;"><script type="text/javascript" src="./files/converter.php"></script><marquee behavior="scroll" scrollamount="2" direction="left" onmouseover="stop()" onmouseout="start()"><span style=" padding-left:10px;">Exchange Rates: </span><span style="padding-left:10px;">USD/EUR</span> = <span style="padding-left:2px;">0.8089</span><span style="padding-left:10px;">USD/GBP</span> = <span style="padding-left:2px;">0.7077</span><span style="padding-left:10px;">USD/JPY</span> = <span style="padding-left:2px;">104.729</span><span style="padding-left:10px;">USD/CHF</span> = <span style="padding-left:2px;">0.9469</span><span style="padding-left:10px;">USD/CAD</span> = <span style="padding-left:2px;">1.2889</span><span style="padding-left:10px;">USD/AUD</span> = <span style="padding-left:2px;">1.2985</span><span style="padding-left:10px;">EUR/GBP</span> = <span style="padding-left:2px;">0.8749</span><span style="padding-left:10px;">EUR/JPY</span> = <span style="padding-left:2px;">129.4709</span><span style="padding-left:10px;">EUR/AUD</span> = <span style="padding-left:2px;">1.6053</span><span style="padding-left:10px;">EUR/RUB</span> = <span style="padding-left:2px;">70.7417</span><span style="padding-left:10px;">GBP/JPY</span> = <span style="padding-left:2px;">147.9913</span><span style="padding-left:10px;"><a href="https://www.exchangeratewidget.com/forex-rates-ticker-widget/" rel="nofollow">Forex Rates Ticker Widget</a></span></marquee></div>
+</div>
+
+				<div class="row" style="margin-top: 5%">
+	<div class="col-lg-3 col-xs-6">
+		<!-- small box -->
+		<div class="small-box bg-aqua">
+			<div class="inner">
+				<h3><i class="fa fa">$</i> 0</h3>
+
+				<p>Available Balance</p>
+			</div>
+			<div class="icon">
+				<i class="fa fa">$</i>
+			</div>
+			<a href="https://Keystockoptions.com/user/index.php#" class="small-box-footer">No Tradings Yet</a>
+		</div>
+	</div>
+	<div class="col-lg-3 col-xs-6">
+		<!-- small box -->
+		<div class="small-box bg-yellow">
+			<div class="inner">
+				<h3><i class="fa fa-user"></i> 4513</h3>
+
+				<p>Register Users</p>
+			</div>
+			<div class="icon">
+				<i class="fa fa-user"></i>
+			</div>
+			<a href="https://Keystockoptions.com/user/index.php#" class="small-box-footer">We provide service you can trust</a>
+		</div>
+	</div>
+	<div class="col-lg-3 col-xs-6">
+		<!-- small box -->
+		<div class="small-box bg-teal">
+			<div class="inner">
+				<h3>100%</h3>
+
+				<p>Client Trust</p>
+			</div>
+			<div class="icon">
+				<i class="fa fa-check"></i>
+			</div>
+			<a href="https://Keystockoptions.com/user/index.php#" class="small-box-footer">Our Services Speaks for us.</a>
+		</div>
+	</div>
+	<div class="col-lg-3 col-xs-6">
+		<!-- small box -->
+		<div class="small-box bg-green">
+			<div class="inner">
+				<h3>197.234.35.2</h3>
+
+				<p>Your IP Address</p>
+			</div>
+			<div class="icon">
+				<i class="fa fa-lock"></i>
+			</div>
+			<a href="https://Keystockoptions.com/user/index.php#" class="small-box-footer">Your access IP right now</a>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg-3 col-xs-6">
+		<div class="info-box bg-red">
+            <span class="info-box-icon"><i class="fa fa">$</i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Invested</span>
+              <span class="info-box-number"> 0</span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 100%"></div>
+              </div>
+                  <span class="progress-description">
+                    N/A                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+	</div>
+	<div class="col-lg-3 col-xs-6">
+		<div class="info-box bg-aqua">
+            <span class="info-box-icon"><i class="fa fa">$</i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Type</span>
+              <span class="info-box-number">0.00</span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 100%"></div>
+              </div>
+                  <span class="progress-description">
+                   Account Type
+                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+	</div>
+	<div class="col-lg-3 col-xs-6">
+		<div class="info-box bg-green">
+            <span class="info-box-icon"><i class="fa fa-map-o"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Location</span>
+              <span class="info-box-text">Nigeria</span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 100%"></div>
+              </div>
+                  <span class="progress-description">
+                    <small>25th March, 2018</small>                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+	</div>
+	<div class="col-lg-3 col-xs-6">
+		<div class="info-box bg-yellow">
+            <span class="info-box-icon"><i class="fa fa-level-up"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Beginner</span>
+              <span class="info-box-text">Trade Rank</span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 50%"></div>
+              </div>
+                  <span class="progress-description">
+                   $ 50K to Upgrade
+                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+	</div>
+
+	
+</div>
+				<!-- /.row -->
+
+
+<script type="text/javascript" src="./files/tv.js"></script>
+<script type="text/javascript">
+new TradingView.widget({
+  "width": 980,
+  "height": 610,
+  "symbol": "NASDAQ:AAPL",
+  "interval": "15",
+  "timezone": "Etc/UTC",
+  "theme": "Light",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "rgba(0, 0, 0, 1)",
+  "enable_publishing": false,
+  "allow_symbol_change": true,
+  "hideideas": true,
+  "news": [
+            "headlines"
+        ]
+});
+</script><div id="tradingview_3b843-wrapper" style="position: relative;box-sizing: content-box;width: 980px;height: 610px;margin: 0 auto !important;padding: 0 !important;font-family:Arial,sans-serif;"><div style="width: 980px;height: 610px;background: transparent;padding: 0 !important;"><iframe id="tradingview_3b843" src="./files/saved_resource.html" style="width: 100%; height: 100%; margin: 0 !important; padding: 0 !important;" frameborder="0" allowtransparency="true" scrolling="no" allowfullscreen=""></iframe></div></div>
+			</section>
+			<div class="control-sidebar-bg"></div>
+
+		</div>
+		<!-- ./wrapper -->
+	</div><style>.tradingview-widget-copyright {         font-size: 13px !important;         line-height: 32px !important;       text-align: center !important;      vertical-align: middle !important;      font-family: 'Trebuchet MS', Tahoma, Arial, sans-serif !important;      color: #9db2bd !important;  }   .tradingview-widget-copyright .blue-text {      color: #3bb3e4 !important;  }   .tradingview-widget-copyright a {       text-decoration: none !important;       color: #9db2bd !important;  }   .tradingview-widget-copyright a:visited {       color: #9db2bd !important;  }   .tradingview-widget-copyright a:hover .blue-text {      color: #38acdb !important;  }   .tradingview-widget-copyright a:active .blue-text {         color: #299dcd !important;  }   .tradingview-widget-copyright a:visited .blue-text {        color: #3bb3e4 !important;  }</style>
+	<footer class="main">
+<br>          
+        <center>    © 2018 || <strong>Keystockoptions ||</strong> Binary Trading Platform
+        </center>
+        </footer>
+
+        <script type="text/javascript">
+    
+    function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  alert("Address Copied");
+  $temp.remove();
+}
+</script>
+
+        <script src="./files/jquery.min.js"></script>
+        <script src="./files/bootstrap.min.js"></script>
+        <script src="./files/fastclick.js"></script>
+        <script src="./files/adminlte.min.js"></script>
+        <script src="./files/jquery.sparkline.min.js"></script>
+        <script src="./files/jquery-jvectormap-1.2.2.min.js"></script>
+        <script src="./files/jquery-jvectormap-world-mill-en.js"></script>
+        <script src="./files/jquery.slimscroll.min.js"></script>
+        <script src="./files/Chart.js"></script>
+        <script src="./files/dashboard2.js"></script>
+        <script src="./files/demo.js"></script>
+        <script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5aa7195dd7591465c70880a2/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+        
+   
+
+
+
+
+
+
+
