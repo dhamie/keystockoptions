93639b63afe3035d631b9ba1d81a687d8ba7f277
root <root@server.keystockoptions.com>
2018-04-12 16:06:06 +0000

    eniola

--- link.php
+++ link.php
@@ -1,15 +1,19 @@
 <?php
-	$connection = mysqli_connect('localhost', 'root', '');
+	$connection = mysqli_connect('localhost', 'root', 'books');
 	if (!$connection){
     	die("Database Connection Failed" . mysqli_error($connection));
 	}else{
-		//echo "connected";
+		echo "connected";
 	}
 	$select_db = mysqli_select_db($connection, 'ay');
 	if (!$select_db){
     die("Database Selection Failed" . mysqli_error($connection));
 	}
 
+	function connect(){ 
+		$db=new mysqli("localhost","root","","books");//philli 4:13
+		return $db;
 
+	}
 
 ?>
